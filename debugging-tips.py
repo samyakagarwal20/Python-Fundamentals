e0a28e101c3b57dfa027f8ccc9f3f0b27ba6785b
Samyak Agarwal <40698105+samyakagarwal20@users.noreply.github.com>
2023-08-06 03:31:06 +0530

    Delete debugging-tips.py

--- debugging-tips.py
+++
@@ -1,9 +0,0 @@
-# We can check the methods availaable for a particular variable by using the dir() method
-message = 'hello world'
-print(dir(message))
-      
-# To get the help regarding any class, we can use the help() method
-print(help(str))
-
-# We can also search for a specific method of a class
-print(help(str.find))
